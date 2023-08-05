91bc6075bac965b1bbad42ee08e89dd25f4f3505
Ivan <kovtun65sergey@gmail.com>
2023-08-05 16:51:27 +0300

    class work 5

---
+++ js/app5-1.js
@@ -0,0 +1,15 @@
+let students = [
+    'Kyle',
+    'Brian',
+    'Frank',
+    'John',
+    'Kate',
+]
+
+for(let i = 0; i < students.length; i++){
+    console.log(i);
+    document.write(students[i]);
+}
+// document.write(students);
+document.write(students.length);
+// document.write('<a href="#">' + 'try link' + '</a>');
