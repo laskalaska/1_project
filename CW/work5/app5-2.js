1fc104b152ed20ad7f5fc8d59960b4dcd0f8e04a
Ivan <kovtun65sergey@gmail.com>
2023-08-10 19:15:24 +0300

    working on 5 practical tasks pt3

--- CW/work5/app5-2.js
+++ CW/work5/app5-2.js
@@ -10,7 +10,7 @@
 //     Знайти добуток позитивних елементів.
 //     Знайти найбільший серед елементів масиву, решту занулити.
 
-let myArr = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+let myArr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
 
 let sumPositive = 0;
 let countPositive = 0;
@@ -22,10 +22,22 @@ let minIndex = 0;
 let maxElement = myArr[0];
 let maxIndex = 0;
 
-for (let i = 0; i < myArr.length; i++){
-    if (myArr[i] > 0){
+let countPositiveNotPair = 0;
+let countPositivePair = 0;
+let sumPositiveNotPair = 0;
+let sumPositivePair = 0;
+
+for (let i = 0; i < myArr.length; i++) {
+    if (myArr[i] > 0) {
         sumPositive += myArr[i]
         countPositive++;
+        if (myArr[i] % 2) {
+            countPositiveNotPair++;
+            sumPositiveNotPair += myArr[i];
+        } else if (!(myArr[i] % 2)) {
+            countPositivePair++;
+            sumPositivePair += myArr[i];
+        }
     } else if (myArr[i] < 0) {
         countNegative++;
     }
@@ -51,3 +63,7 @@ console.log(`Sum of positive numbers = ${sumPositive}, count of positive numbers
 console.log(`Minimal value of the array is ${minElement}, its index is ${minIndex}`);
 console.log(`Maximum value of the array is ${maxElement}, its index is ${maxIndex}`);
 console.log(`Count of negative numbers = ${countNegative}`);
+console.log(`Count of NOT pair positive numbers = ${countPositiveNotPair}`);
+console.log(`Count of pair positive numbers = ${countPositivePair}`);
+console.log(`Sum of pair positive numbers = ${sumPositivePair}`);
+console.log(`Sum of NOT pair positive numbers = ${sumPositiveNotPair}`);
