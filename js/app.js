87045cb1b378b554c84d817bc90f084fc9e9d1cb
Ivan <kovtun65sergey@gmail.com>
2023-08-14 19:30:37 +0300

    working on 6 homework tasks pt 1

--- js/app.js
+++ js/app.js
@@ -1,34 +1,25 @@
 const startDiscountFrom = 10000;
 const discount = 10;
 
-const disountValue = (100 - discount) / 100;
+const discountValue = (100 - discount) / 100;
 
 showProducts();
 
-
-
 // Get product number
-let productNumber;
-do {
-  productNumber = parseInt(prompt('Enter product number which you wanna buy:'));
-} while(productNumber < 1 || productNumber > products.length || isNaN(productNumber));
+const productNumber = getProductNumber();
 
 // Get product amount
-let productsAmount;
-do {
-  productsAmount = parseInt(prompt('Enter products amount:'));
-} while(productsAmount < 1 || isNaN(productsAmount));
+const productsAmount = getProductAmount();
 
 // save selected product
-const selectedProduct = products[productNumber - 1];
+const selectedProduct = getSelectedProduct(productNumber);
 
 // Calculate initial price without discount
-let initialPrice = selectedProduct.price * productsAmount;
-console.log('Price: $', initialPrice);
+calcPrice(selectedProduct, productsAmount);
 
 // Calculate price with discount if needed
-if (initialPrice >= startDiscountFrom) {
-  const finalPrice = initialPrice * discountValue;
-  console.log('Congrats! You got a discount, the final price is $' + finalPrice);
-}
+// if (initialPrice >= startDiscountFrom) {
+//   const finalPrice = initialPrice * discountValue;
+//   console.log('Congrats! You got a discount, the final price is $' + finalPrice);
+// }
 
--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,39 @@ function showProducts() {
     console.log(`#${(i + 1)} Product: ${products[i].name} | Price: $${products[i].price}`);
   }
 }
+
+function getProductNumber () {
+  let productNumber;
+  do {
+    productNumber = parseInt(prompt('Enter product number which you wanna buy:'));
+  } while(productNumber < 1 || productNumber > products.length || isNaN(productNumber));
+
+  return productNumber;
+}
+
+function getProductAmount () {
+  let productsAmount;
+  do {
+    productsAmount = parseInt(prompt('Enter products amount:'));
+  } while(productsAmount < 1 || isNaN(productsAmount));
+
+  return productsAmount;
+}
+
+function getSelectedProduct (productNumber) {
+  return products[productNumber - 1];
+}
+
+function calcPrice (selectedProduct, productsAmount) {
+  let initialPrice = selectedProduct.price * productsAmount;
+  console.log('Price: $', initialPrice);
+
+  if (isDiscount(initialPrice)) {
+    const finalPrice = initialPrice * discountValue;
+    console.log('Congrats! You got a discount, the final price is $' + finalPrice);
+  }
+}
+
+function isDiscount (initialPrice) {
+  return initialPrice >= startDiscountFrom;
+}
