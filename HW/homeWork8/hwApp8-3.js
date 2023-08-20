30daaab1c52883fda302c90a5004190a6c01a31f
Ivan <kovtun65sergey@gmail.com>
2023-08-20 21:11:35 +0300

    working on 8 homework tasks pt 2 3/3 is done complete

---
+++ HW/homeWork8/hwApp8-3.js
@@ -0,0 +1,47 @@
+// The ladder (сходи) – об'єкт, який дозволяє підійматися вгору та спускатися:
+//
+// let ladder = {
+//     step: 0,
+//     up: function () {
+//         this.step++;
+//     },
+//     down: function () {
+//         this.step--;
+//     },
+//     showStep: function () { // показує теперішню сходинку
+//         alert(this.step);
+//     }
+// };
+// Тепер, якщо нам потрібно зробити кілька послідовних викликів, ми можемо виконати це так:
+//
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
+// Змініть код методів up, down і showStep таким чином, щоб їх виклик можна було зробити по ланцюжку, наприклад:
+//
+// ladder.up().up().down().showStep(); // 1
+// Такий підхід широко використовується в бібліотеках JavaScript.
+
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () { // показує теперішню сходинку
+        alert(this.step);
+        return this;
+    }
+};
+
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+
+ladder.up().up().down().showStep();
