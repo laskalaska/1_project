f4c26758ee8bb9c7ba39d0f03078ebf667ab108d
Ivan <kovtun65sergey@gmail.com>
2023-09-27 00:44:30 +0300

    did bootstrap tasks in 16 homework

---
+++ HW/homeWork16/js/app.js
@@ -0,0 +1,43 @@
+// const openModalBtn = document.getElementById('openModalBtn');
+// console.log(openModalBtn);
+
+// const myModal = document.getElementById('myModal')
+// const myInput = document.getElementById('myInput')
+//
+// myModal.addEventListener('shown.bs.modal', () => {
+//     myInput.focus()
+// })
+// import bootstrap from "bootstrap"
+// window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+
+// Enable tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// const alertList = document.querySelectorAll('.alert')
+// const alerts = [...alertList].map(element => new bootstrap.Alert(element))
+
+// Alert init
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        if (!(document.querySelector('[role="alert"]'))) {
+            appendAlert('Nice, you triggered this alert message!', 'success')
+        } else {
+            document.querySelector('[role="alert"]').remove();
+        }
+    })
+}
