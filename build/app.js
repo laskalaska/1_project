bd7b7473e54c2d31b0a620e2a8f0150dce4b5090
Ivan <kovtun65sergey@gmail.com>
2023-10-01 23:45:59 +0300

    install babel packets and added it to project files

---
+++ build/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var x = 0;
+var f = function f() {
+  return x + 10;
+};
+console.log(f());
+console.log(f());
+console.log(f());
+var js = 'javascript',
+  php = 'php',
+  py = 'python',
+  rb = 'ruby';
+console.log(js);
+console.log(php);
+console.log(py);
+console.log(rb);
