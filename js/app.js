69be10f368644718ab5768ae82d6b62110579b94
Ivan <kovtun65sergey@gmail.com>
2023-10-01 23:45:44 +0300

    install babel packets and added it to project files

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let x = 0;
+
+let f = () => {
+    return x + 10;
+}
+console.log(f());
+console.log(f());
+console.log(f());
+
+let [js, php, py, rb] = ['javascript', 'php', 'python', 'ruby'];
+
+console.log(js);
+console.log(php);
+console.log(py);
+console.log(rb);
